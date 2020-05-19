32e49248a2b152c6928ade9b4a46083b1fd231b3
Sophie 'Sharky' Schumann <sharky@sharky.pw>
2020-05-19 14:48:31 +0200

    Made some things nicer

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ fn print_help() {
     println!("Requires one argument:\tinfile - File to convert to PNG");
 }
 
-fn main() -> Result<(),std::io::Error> {
+fn main() -> Result<(),Box<dyn std::error::Error>> {
     println!("Hello, world!");
 
     if env::args().len() <= 1 {
@@ -22,13 +22,12 @@ fn main() -> Result<(),std::io::Error> {
     let args: Vec<String> = env::args().collect();
 
     let infilepath = Path::new(&args[1]);
-    let outfilepath = format!("{}.png", infilepath.file_stem().unwrap().to_str().unwrap());
+    let outfilepath = infilepath.with_extension("png");
 
     println!("Trying to read '{}' ...", infilepath.display());
 
     let infile = std::fs::File::open(infilepath)?;
-    #[allow(non_snake_case)]
-    let mut infileReader = std::io::BufReader::new(&infile);
+    let mut infile_reader = std::io::BufReader::new(&infile);
 
     let filelen = infile.metadata().unwrap().len();
     println!("Filelen is {} bytes ...", filelen);
@@ -48,11 +47,13 @@ fn main() -> Result<(),std::io::Error> {
 
     for y in 0..height {
         for x in 0..width {
-            infileReader.read(&mut buf).expect("read failed");
+            infile_reader.read(&mut buf).expect("read failed");
             img.put_pixel(x, y, Rgb(buf));
         }
     }
 
+    println!("Writing image to '{}' ...", outfilepath.to_str().unwrap());
     img.save(outfilepath).expect("unable to save image?");
+    println!("Done!");
     Ok(())
 }
